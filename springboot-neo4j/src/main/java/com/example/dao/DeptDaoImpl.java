3aab86ff9a5841c07289fbb11e1e2c0bf41cf5ff
Jitesh Pathak <Jitesh Pathak@DESKTOP-MUBC4CB>
2018-03-16 16:00:15 +0530

    --

--- springboot-neo4j/src/main/java/com/example/dao/DeptDaoImpl.java
+++ springboot-neo4j/src/main/java/com/example/dao/DeptDaoImpl.java
@@ -4,7 +4,11 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.Model.Department;
-
+/**
+ * 
+ * @author Jitesh
+ *
+ */
 @Repository("DeptDao")
 @Transactional
 public class DeptDaoImpl implements DeptDao{
